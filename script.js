533c183cc533a9c751394beec9f3dfabfaeef7d6
dweisel <drewweisel@gmail.com>
2023-06-06 15:25:02 -0600

    updates

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-// Function to generate a random word
-function generateRandomWord() {
+function getRandomWord() {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
-  const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  const consonants = 'bcdfghjklmnpqrstvwxyz';
 
-  // Randomly select a vowel for the middle position
-  const middleVowel = vowels[Math.floor(Math.random() * vowels.length)];
-
-  // Randomly select a consonant for the first position
-  let firstConsonant = consonants[Math.floor(Math.random() * consonants.length)];
-
-  // Randomly select a consonant for the third position
-  let thirdConsonant = consonants[Math.floor(Math.random() * consonants.length)];
-
-  // Combine the three parts to form the word
-  const word = firstConsonant + '<span class="vowel">' + middleVowel + '</span>' + thirdConsonant;
+  let word = '';
+  word += consonants[Math.floor(Math.random() * consonants.length)];
+  word += vowels[Math.floor(Math.random() * vowels.length)];
+  word += consonants[Math.floor(Math.random() * consonants.length)];
 
   return word;
 }
 
-// Generate a random word
-let randomWord = generateRandomWord();
-
-// Display the word on the page
 const wordContainer = document.getElementById('wordContainer');
-wordContainer.innerHTML = randomWord;
+const word = getRandomWord();
+const formattedWord = `<span class="vowel">${word[0]}</span>${word[1]}<span class="vowel">${word[2]}</span>`;
+wordContainer.innerHTML = formattedWord;
 
-// Reset button event handler
-const resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', function() {
-  randomWord = generateRandomWord();
-  wordContainer.innerHTML = randomWord;
-});
