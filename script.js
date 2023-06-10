aede5d76825e8342e8fdde0e5a353fd3a951a099
dweisel <drewweisel@gmail.com>
2023-06-10 17:55:38 -0600

    added a drop down button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let rightButtonClickCount = 0;
 let wrongButtonClickCount = 0;
+let selectedWordCount = 0;
+let generatedWordCount = 0;
 
 const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
@@ -18,6 +20,8 @@ const generateRandomWord = (isRightButton) => {
   const randomWordContainer = document.getElementById('random-word');
   randomWordContainer.innerHTML = randomWord;
   
+  generatedWordCount++;
+  
   if (isRightButton) {
     rightButtonClickCount++;
   } else {
@@ -26,9 +30,29 @@ const generateRandomWord = (isRightButton) => {
   
   console.log(`Right Button Click Count: ${rightButtonClickCount}`);
   console.log(`Wrong Button Click Count: ${wrongButtonClickCount}`);
+  
+  if (generatedWordCount === selectedWordCount) {
+    displayButtonStatistics();
+  }
 };
 
-// Generate and display a random word on page load
+const displayButtonStatistics = () => {
+  const percentage = ((rightButtonClickCount / generatedWordCount) * 100).toFixed(2);
+  const statisticsContainer = document.getElementById('statistics');
+  statisticsContainer.innerHTML = `Right Button Click Count: ${rightButtonClickCount}<br>
+                                    Wrong Button Click Count: ${wrongButtonClickCount}<br>
+                                    Percentage of Right Words: ${percentage}%`;
+};
+
+const selectWordCount = (event) => {
+  selectedWordCount = parseInt(event.target.value);
+  generatedWordCount = 0;
+  rightButtonClickCount = 0;
+  wrongButtonClickCount = 0;
+  document.getElementById('statistics').innerHTML = '';
+};
+
 window.addEventListener('DOMContentLoaded', () => {
-  generateRandomWord(false); // Pass false since it's not a button click
+  generateRandomWord(false);
 });
+
